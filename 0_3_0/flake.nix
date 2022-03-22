583a08fff7f77aef27f18bc9d4bfa4c475091e98
hugosenari <hugosenari@gmail.com>
2022-03-22 00:25:51 -0300

    chore: re-index ../nimpkgs/c/checkpack/0_3_0

--- 0_3_0/flake.nix
+++ 0_3_0/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-checkpack-0_3_0.flake = false;
   inputs.src-checkpack-0_3_0.owner = "EchoPouet";
-  inputs.src-checkpack-0_3_0.ref   = "refs/tags/0.3.0";
+  inputs.src-checkpack-0_3_0.ref   = "0_3_0";
   inputs.src-checkpack-0_3_0.repo  = "checkpack";
   inputs.src-checkpack-0_3_0.type  = "gitlab";
   
